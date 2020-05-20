be5e68d49d51ca4a9c0a8b5a301f451aeac29707
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2020-05-20 21:33:00 +0300

    Re-download if data has changed remotely

--- R/plan.R
+++ R/plan.R
@@ -1,6 +1,13 @@
+
+# Define CDDA target URL
+cdda_url <- "https://www.eea.europa.eu/data-and-maps/data/nationally-designated-areas-national-cdda-14/cdda/cdda-csv-files/at_download/file"
+
 the_plan <-
   drake_plan(
   ## Plan targets in here.
-  csv = get_data(),
+  csv = target(
+    get_data(cdda_url),
+    trigger = trigger(change = HEAD(cdda_url)$headers[["last-modified"]])
+  ),
   data = load_data(csv)
 )
