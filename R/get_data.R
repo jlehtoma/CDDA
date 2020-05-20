11888c0435b2180bef7ce7f35fe658e25657f07f
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2020-05-20 21:32:43 +0300

    Use curl

--- R/get_data.R
+++ R/get_data.R
@@ -4,14 +4,12 @@
 ##'
 ##' @title
 
-get_data <- function() {
-  # Define CDDA target URL
-  target_url <- "https://www.eea.europa.eu/data-and-maps/data/nationally-designated-areas-national-cdda-14/cdda/cdda-csv-files/at_download/file"
+get_data <- function(target_url) {
   # Create data dir if it doesn't exist
   dir_create("data")
   # Download data
   target_file = "data/CDDA_data.zip"
-  download.file(target_url, destfile = target_file, mode = 'wb')
+  curl_download(target_url, target_file)
   # Unzip data, the result will be a csv file path
   unzip(target_file, exdir = "data")
 }
